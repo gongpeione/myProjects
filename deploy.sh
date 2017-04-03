e7e91b6890b7e6bb1e564fb5781770900256c0f3
Geeku <ggongpei@qq.com>
2017-04-03 16:51:33 +0800

    github icon

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+npm run build
+git add .
+git status
+git commit -m "$1"
+git push
--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 /**
  * Created by geeku on 01/04/2017.
  */
+let curPopup = null;
+document.addEventListener('click', () => {
+	if (curPopup && curPopup.classList.contains('popup')) {
+		curPopup.style.cssText = '';
+		curPopup.classList.remove('popup');
+		curPopup = null;
+	}
+})
 new Vue({
 	el: '#project',
 	data: {
@@ -14,6 +22,42 @@ new Vue({
 			.then((data) => {
 				this.lists = data;
 			});
+	},
+	methods: {
+		popup (e) {
+			let li = e.target;
+			if (li.classList.contains('noPop')) {
+				return;
+			}
+			while (li.tagName !== 'LI') {
+				li = li.parentNode;
+			}
+			if (curPopup && curPopup.classList.contains('popup') && curPopup !== li) {
+				curPopup.style.cssText = '';
+				curPopup.classList.remove('popup');
+			}
+			curPopup = li;
+			if (li.classList.contains('popup')) {
+				li.style.cssText = '';
+				li.classList.remove('popup');
+				return;
+			}
+			const winWidth = window.innerWidth;
+			const winHeight = window.innerHeight;
+			let rect = li.getBoundingClientRect();
+			rect = {
+				x: rect.left,
+				y: rect.top,
+				width: rect.width,
+				height: rect.height
+			}
+
+			const translateX = ((winWidth - rect.width) / 2) - rect.x;
+			const translateY = ((winHeight - rect.height) / 2) - rect.y;
+			li.style.cssText = `transform: translate(${translateX}px, ${translateY}px) scale(2);`;
+
+			li.classList.add('popup');
+		}
 	},
 	computed: {
 		listFilted () {
